6bc2bfab2e3e540f9b46414993f61920ed0b98cf
yangryu321 <yangryu321@gmail.com>
2022-04-17 19:25:10 -0400

    -Added extension methods for app

--- minimalAPI/Program.cs
+++ minimalAPI/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using minimalAPI.DataAccess;
-using minimalAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DataContext>(options => {
@@ -9,50 +8,7 @@ builder.Services.AddDbContext<DataContext>(options => {
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapUsersApi();
 
-app.MapGet("/Users", async (DataContext dataContext) =>
-{
-    return await dataContext.Users.ToListAsync();
-});
-
-app.MapGet("/Users/{Id:int}", async (DataContext datacontext, int Id) =>
-{
-    return await datacontext.Users.FindAsync(Id);
-
-});
-
-app.MapPost("/Users", async (DataContext datacontext, User user) => 
-{ 
-    await datacontext.Users.AddAsync(user);
-    await datacontext.SaveChangesAsync();
-    Results.Accepted();
-
-});
-
-app.MapPut("/Users/{Id:int}", async (DataContext datacontext, int Id, User user) =>
-{
-    if (Id != user.Id)
-        return Results.BadRequest();
-
-    datacontext.Update(user);
-    await datacontext.SaveChangesAsync();
-
-    return Results.NoContent(); 
-}
-);
-
-app.MapDelete("/Users/{Id:int}", async (DataContext dataContext, int Id) =>
-{
-    var user = await dataContext.Users.FindAsync(Id);
-    if (user is null)
-        return Results.NotFound();
-
-    dataContext.Users.Remove(user);
-    await dataContext.SaveChangesAsync();
-
-    return Results.NoContent();
-
-});
 
 app.Run();
---
+++ minimalAPI/UserApiExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using minimalAPI.DataAccess;
+using minimalAPI.Models;
+
+public static class UserApiExtensions
+{ 
+
+    public static WebApplication MapUsersApi(this WebApplication app)
+    {
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.MapGet("/Users", async (DataContext dataContext) =>
+        {
+            return await dataContext.Users.ToListAsync();
+        });
+
+        app.MapGet("/Users/{Id:int}", async (DataContext datacontext, int Id) =>
+        {
+            return  await datacontext.Users.FindAsync(Id);
+           
+
+        });
+
+        app.MapPost("/Users", async (DataContext datacontext, User user) =>
+        {
+            await datacontext.Users.AddAsync(user);
+            await datacontext.SaveChangesAsync();
+            Results.Accepted();
+
+        });
+
+        app.MapPut("/Users/{Id:int}", async (DataContext datacontext, int Id, User user) =>
+        {
+            if (Id != user.Id)
+                return Results.BadRequest();
+
+            datacontext.Update(user);
+            await datacontext.SaveChangesAsync();
+
+            return Results.NoContent();
+        }
+        );
+
+        app.MapDelete("/Users/{Id:int}", async (DataContext dataContext, int Id) =>
+        {
+            var user = await dataContext.Users.FindAsync(Id);
+            if (user is null)
+                return Results.NotFound();
+
+            dataContext.Users.Remove(user);
+            await dataContext.SaveChangesAsync();
+
+            return Results.NoContent();
+
+        });
+        return app;
+
+    }
+}
