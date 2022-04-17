110af485f52c83e6483bee1cac1d4e9e3fa32504
yangryu321 <yangryu321@gmail.com>
2022-04-17 18:54:12 -0400

    -Added post put delete api

--- minimalAPI/Program.cs
+++ minimalAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using minimalAPI.DataAccess;
+using minimalAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DataContext>(options => {
@@ -10,9 +11,48 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/Users", (DataContext dataContext) =>
+app.MapGet("/Users", async (DataContext dataContext) =>
 {
-    return dataContext.Users.ToListAsync();
+    return await dataContext.Users.ToListAsync();
+});
+
+app.MapGet("/Users/{Id:int}", async (DataContext datacontext, int Id) =>
+{
+    return await datacontext.Users.FindAsync(Id);
+
+});
+
+app.MapPost("/Users", async (DataContext datacontext, User user) => 
+{ 
+    await datacontext.Users.AddAsync(user);
+    await datacontext.SaveChangesAsync();
+    Results.Accepted();
+
+});
+
+app.MapPut("/Users/{Id:int}", async (DataContext datacontext, int Id, User user) =>
+{
+    if (Id != user.Id)
+        return Results.BadRequest();
+
+    datacontext.Update(user);
+    await datacontext.SaveChangesAsync();
+
+    return Results.NoContent(); 
+}
+);
+
+app.MapDelete("/Users/{Id:int}", async (DataContext dataContext, int Id) =>
+{
+    var user = await dataContext.Users.FindAsync(Id);
+    if (user is null)
+        return Results.NotFound();
+
+    dataContext.Users.Remove(user);
+    await dataContext.SaveChangesAsync();
+
+    return Results.NoContent();
+
 });
 
 app.Run();
